f35fb7d7ab4f2b728b3a4426460f4a2870b0ddfa
Jean Vitor Vieira <jeanvitorv0@unesc.net>
2025-07-17 16:53:04 -0300

    Implementando UserDetailsService e configurando autenticacao via banco de dados (WIP)

---
+++ src/main/java/com/javanauta/cadastro_usuario/service/security/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.javanauta.cadastro_usuario.service.security;
+
+import com.javanauta.cadastro_usuario.infrastructure.entities.Usuario;
+import com.javanauta.cadastro_usuario.infrastructure.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UserDetailsServiceImpl(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com o e-mail: " + email));
+
+        return new User(usuario.getEmail(), usuario.getPassword(), new ArrayList<>());
+    }
+}
